458a190c7ea0e729d0e5734ad8bb6fd73a56ad74
Ivan Prokopiev <ProkopievJohn@gmal.com>
2016-09-19 14:20:38 +0300

    before check

--- script_components/AddDelMusician.js
+++ script_components/AddDelMusician.js
@@ -5,8 +5,12 @@ function AddDelMusician(el) {
 }
 
 AddDelMusician.prototype.init = function () {
+	this.ADEl.addEventListener('keyup', this.activeBtn.bind(this, this.ADEl.querySelector('.input'), this.ADEl.querySelector('.my-btn')));
+	this.ADEl.addEventListener('click', this.adFunc.bind(this));
+};
 
-	this.ADEl.addEventListener('click', this.adFunc.bind(this))
+AddDelMusician.prototype.activeBtn = function (input, myBtn) {
+	input.value !== '' ? myBtn.removeAttribute('disabled') :  myBtn.setAttribute('disabled', 'disabled');
 };
 
 AddDelMusician.prototype.adFunc = function (e) {
@@ -15,7 +19,7 @@ AddDelMusician.prototype.adFunc = function (e) {
 	if (target.classList.contains('my-btn')) {
 		this.addMusician(this.ADEl.querySelector('.input'));
 	} else if (target.classList.contains('list')) {
-		target.classList.toggle('for-del');
+		this.toggleClass(target, this.ADEl.querySelector('.btn-del'));
 	} else if (target.classList.contains('btn-del')) {
 		this.delMusician();
 	}
@@ -23,15 +27,20 @@ AddDelMusician.prototype.adFunc = function (e) {
 
 AddDelMusician.prototype.addMusician = function (input) {
 	if (input.value == '') return;
-
 	document.querySelector('.musicians').insertAdjacentHTML('beforeend', '<li class="list">' + input.value + '</li>');
-
 	input.value = '';
 };
 
-AddDelMusician.prototype.delMusician = function () {
-	var listForDel = this.ADEl.querySelectorAll('.for-del');
+AddDelMusician.prototype.toggleClass = function (target, btnDel) {
+	target.classList.toggle('for-del');
 
+	var listForDel = this.ADEl.querySelectorAll('.for-del');
+	listForDel.length !== 0 ?  btnDel.removeAttribute('disabled') :  btnDel.setAttribute('disabled', 'disabled');
+};
+
+AddDelMusician.prototype.delMusician = function () {
+	if (!confirm('question')) return;
+	var listForDel = this.ADEl.querySelectorAll('.for-del');
 	for (var i = 0; i < listForDel.length; i++) {
 		listForDel[i].parentNode.removeChild(listForDel[i]);
 	}
